ebf61627bc21dfcc4bf22c2026f3f02838c810e6
Murat Emre Sargın <96112076+Abbys-Murat@users.noreply.github.com>
2023-12-25 21:42:26 +0300

    Add files via upload

---
+++ proje/Models/Adress.cs
@@ -0,0 +1,14 @@
+﻿namespace proje.Models
+{
+    public class Adress
+    {
+        [Key]
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        public string City { get; set; }
+
+        public string State { get; set; }
+    }
+}
