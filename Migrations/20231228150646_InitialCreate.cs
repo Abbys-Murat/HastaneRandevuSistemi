df9177ee995a54d0056c695fb4e4f9164b2043b4
Murat Emre Sargın <96112076+Abbys-Murat@users.noreply.github.com>
2023-12-28 22:40:18 +0300

    Add files via upload

---
+++ Migrations/20231228150646_InitialCreate.cs
@@ -0,0 +1,139 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace proje.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Addresses",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    City = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    State = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Street = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Addresses", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AppUser",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    AdresssId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AppUser", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_AppUser_Addresses_AdresssId",
+                        column: x => x.AdresssId,
+                        principalTable: "Addresses",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Doktorlar",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    WorkingDays = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    AppUserId = table.Column<string>(type: "nvarchar(450)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Doktorlar", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Doktorlar_AppUser_AppUserId",
+                        column: x => x.AppUserId,
+                        principalTable: "AppUser",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Polikinlikler",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Image = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    AddressId = table.Column<int>(type: "int", nullable: true),
+                    PolikinlikCategoryyId = table.Column<int>(type: "int", nullable: false),
+                    AppUserId = table.Column<string>(type: "nvarchar(450)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Polikinlikler", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Polikinlikler_Addresses_AddressId",
+                        column: x => x.AddressId,
+                        principalTable: "Addresses",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Polikinlikler_AppUser_AppUserId",
+                        column: x => x.AppUserId,
+                        principalTable: "AppUser",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Polikinlikler_Polikinlikler_PolikinlikCategoryyId",
+                        column: x => x.PolikinlikCategoryyId,
+                        principalTable: "Polikinlikler",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AppUser_AdresssId",
+                table: "AppUser",
+                column: "AdresssId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Doktorlar_AppUserId",
+                table: "Doktorlar",
+                column: "AppUserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Polikinlikler_AddressId",
+                table: "Polikinlikler",
+                column: "AddressId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Polikinlikler_AppUserId",
+                table: "Polikinlikler",
+                column: "AppUserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Polikinlikler_PolikinlikCategoryyId",
+                table: "Polikinlikler",
+                column: "PolikinlikCategoryyId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Doktorlar");
+
+            migrationBuilder.DropTable(
+                name: "Polikinlikler");
+
+            migrationBuilder.DropTable(
+                name: "AppUser");
+
+            migrationBuilder.DropTable(
+                name: "Addresses");
+        }
+    }
+}
